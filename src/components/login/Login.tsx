8fdd7e6e351f4672caf8971d31b6c1af89ee5345
damien dumontet <damiendumontet@damiens-MacBook-Pro.local>
2018-11-22 15:37:17 +0100

    Init Login view

--- src/components/login/Login.tsx
+++ src/components/login/Login.tsx
@@ -1,10 +1,43 @@
+import "./Login.css";
+
 import * as React from "react";
+import { Button, Col, Container, FormGroup, Input, Row } from "reactstrap";
 
 class Login extends React.Component {
-	public render() {
+	public render(): React.ReactNode {
 		return (
 			<div className="Login">
-				<h1>Login Page</h1>
+				<Container>
+					<Row className="vertical-center">
+						<Col sm={{ size: 4, order: 2, offset: 2 }}>
+							<FormGroup>
+								<Input
+									type="email"
+									name="email"
+									id="exampleEmail"
+									placeholder="Email"
+								/>
+							</FormGroup>
+						</Col>
+						<Col sm={{ size: 4, order: 2, offset: 0 }}>
+							<FormGroup>
+								<Input
+									type="password"
+									name="password"
+									id="examplePassword"
+									placeholder="Password"
+								/>
+							</FormGroup>
+						</Col>
+					</Row>
+					<Row>
+						<Col sm={{ size: 8, order: 2, offset: 2 }}>
+							<Button size="lg" block={true}>
+								Sign in
+							</Button>
+						</Col>
+					</Row>
+				</Container>
 			</div>
 		);
 	}
